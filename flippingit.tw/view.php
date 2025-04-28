1957531ee5d04c37309d38b86515f787247748b2
hanbz <hanbzsky@yahoo.com.tw>
2025-04-28 12:34:57 +0800

    feat: 新增 robots.txt 檔案並更新 meta 標籤
    - 新增 robots.txt 檔案以禁止所有搜尋引擎爬蟲
    - 在 index.html 和 view.php 中新增 meta 標籤以防止索引和跟隨連結

--- flippingit.tw/view.php
+++ flippingit.tw/view.php
@@ -30,6 +30,7 @@ $localPath = '.' . $path;
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="robots" content="noindex, nofollow">
     <title>相框成品</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
